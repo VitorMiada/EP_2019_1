c8a3b6acca58a4cb5a4653514ea775ff4d8030ad
Sophia <sophia.shigueoka@gmail.com>
2019-04-20 18:04:45 -0300

    Update ep1.py

--- ep1.py
+++ ep1.py
@@ -1,4 +1,3 @@
-
 # Alunos:
 # - aluno Vitor Kenzo Fujiwara Miada, sophiaks@al.insper.edu.br
 # - aluno B: Sophia Kerber Shigueoka, vitorkfm@al.insper.edu.br
@@ -7,7 +6,7 @@ import random
 def carregar_cenarios():
     cenarios = {
         "inicio": {
-            "titulo": "Saguão do perigo",
+            "titulo": "\nSaguão do perigo",
             "descricao": "Você esta no saguão de entrada do Insper",
             "opcoes": {
                 "andar professor": "Tomar o elevador para o andar do professor",
@@ -15,7 +14,7 @@ def carregar_cenarios():
             }
         },
         "andar professor": {
-            "titulo": "Andar do desespero",
+            "titulo": "\nAndar do desespero",
             "descricao": "Voce chegou ao andar da sala do seu professor",
             "opcoes": {
                 "inicio": "Tomar o elevador para o saguão de entrada",
@@ -23,7 +22,7 @@ def carregar_cenarios():
             }
         },
         "professor": {
-            "titulo": "O guardião",
+            "titulo": "\nO guardião",
             "descricao": "Você foi até a porta da sala do professor, porém existem golens guardiões ligados a uma protoboard. "
                          "Você vai falar com o golem. Ele te empurra para trás e não te deixa entrar na sala "
                          "Você precisa de mais itens.",
@@ -32,14 +31,14 @@ def carregar_cenarios():
             }
         },
         "biblioteca": {
-            "titulo": "Caverna da tranquilidade",
+            "titulo": "\nCaverna da tranquilidade",
             "descricao": "Você está na biblioteca. Você vai falar com a bibliotecaria e ela misteriosamente diz: It's dangerous to go alone, TAKE THIS!!. \n Você recebeu o livro 'O golem' ",
             "opcoes": {
                 "aquario": "Pegar um aquário"
             }
         },
         "aquario": {
-            "titulo": "Sala do sonho",
+            "titulo": "\nSala do sonho",
             "descricao": "Você entrou em um aquário e acabou pegando no sono, tendo um sonho no qual não era possivel acordar sem responder uma pergunta: o teletransporte é possível?",
             "opcoes": {
                 "s": "sim",
@@ -47,7 +46,7 @@ def carregar_cenarios():
             }
         },
         "s": {
-            "titulo": "Sebastião Camargo",
+            "titulo": "\nSebastião Camargo",
             "descricao": "Parece que sua resposta estava correta. Você acorda na frente da sala Sebastião Camargo e entra nela. Ihh! Você entrou no meio de uma aula de NatDes.",
             "opcoes": {
                 "voltar": "Sair de fininho e fingir que nada aconteceu",
@@ -56,7 +55,7 @@ def carregar_cenarios():
             }
         },
         "n": {
-            "titulo": "L3",
+            "titulo": "\nL3",
             "descricao" : "Sua resposta estava errada. O teletransporte é possível! Você acorda no L3. Os fornos para calibrar sua termorresistência estão todos nas bancadas.",
             "opcoes": {
                 "exemplar": "Ser um bom aluno e acabar seu relatório",
@@ -64,21 +63,21 @@ def carregar_cenarios():
             }    
         },
         "exemplar": {
-                   "titulo": "Parceria com o Carlinhos",
+                   "titulo": "\nParceria com o Carlinhos",
                    "descricao": "Voce fez perfeitamente seu relatório. Ao explicar sua situação ao Carlos, ele te ajuda, te dando uma termorresistência. Você pega o elevador e vai para o quinto andar. ",
                    "opcoes": {
                         "5": "Ir para o quinto andar"
                     }
                  },
         "desonesto": {
-                    "titulo": "O rei mago de InstruMed",
+                    "titulo": "\nO rei mago de InstruMed",
                     "descricao": "Ao descalibrar todos dos fornos, o Mago Carlinhos te desafia para um duelo.",
                     "opcoes":{
                             "lutar2": "Lute com o Rei Mago!!"
                             }
                     },
                     "lutar2":{
-                            "titulo": "Ida ao elevador",
+                            "titulo": "\nIda ao elevador",
                             "descricao": "Apás a luta você vai para o elevador",
                             "opcoes":{
                                     "1": "Primeiro andar",
@@ -89,29 +88,29 @@ def carregar_cenarios():
                                     }
                             },
         "voltar": {
-                "titulo": "Pego de surpresa",
-                "descricao" : "Você tentou fugir da sala mas o professor te pegou!. \n LUTE COM ELE!!(obs: não lute com seu professor, isso é so um joguinho!)",
+                "titulo": "\nPego de surpresa",
+                "descricao" : "Você tentou fugir da sala mas o professor te pegou! \n LUTE COM ELE! \n(observação: não lute com seu professor, isso é so um joguinho!)",
                 "opcoes": {
                         "lutar1": "lute com o professor"
             }
                        
         },
         "disfarce": {
-                   "titulo": "O mestre do disfarce",
+                   "titulo": "\nO mestre do disfarce",
                    "descricao": "Ao colocar seu protótipo na cabeça, voce percebe que ele serve como uma capa de invisibilidade. Voce ganhou o Protótipo e foi para o quinto andar.",
                    "opcoes":{
                            "5": "Ir para a o quinto andar"
                            }
                    },
         "ousadia": {
-                "titulo": " Not so ousado",
+                "titulo": "\nNot so ousado",
                 "descricao": "Ao dizer que você era o verdadeiro Vitor Macul, todos riem e o verdadeiro Vitor Macul te expulsa da aula. \n Você retornou para a frente da sala.",
                 "opcoes":{
                         "s": "Voltar para voltar pra frente da sala"
                         }
                 },
         "lutar1" :{
-                "titulo":" O elevador",
+                "titulo":"\nO elevador",
                 "descricao" : "Após a luta você vai para o elevador.",
                 "opcoes":{
                         "elevador": "Chamar o elevador",
@@ -119,7 +118,7 @@ def carregar_cenarios():
              }
         },
         "elevador": {
-                "titulo": "O Elevador macabro",
+                "titulo": "\nO Elevador macabro",
                 "descricao": "Você entra no elevador. Para qual andar você deseja ir?",
                 "opcoes":{
                         "1": "primeiro andar",
@@ -130,7 +129,7 @@ def carregar_cenarios():
                         }
                 },
                 "1":{
-                    "titulo": "O primeiro andar",
+                    "titulo": "\nO primeiro andar",
                     "descricao": "Você chega no primeiro andar e se incomoda com alguma coisa....",
                     "opcoes":{
                             "nerdbox": "Entrar no NerdBox",
@@ -138,14 +137,14 @@ def carregar_cenarios():
                             }
                     },
                     "nerdbox":{
-                            "titulo": "A caixa da depressão",
+                            "titulo": "\nA caixa da depressão",
                             "descricao": "Ao adentrar no NerdBox, você encontra o Pelicano. Ele diz que ja tentou alterar a data do EP, mas falhou miseravelmente. Porém, diz acreditar em você, e te dá a espada da modelagem",
                             "opcoes": {
                                     "elevador": "Ir para o elevador"
                                     }
                             },
                     "2":{
-                     "titulo": "O segundo andar",
+                     "titulo": "\nO segundo andar",
                      "descricao": "Você chega no segundo andar e avista o Fernando de GDE",
                      "opcoes": {
                              "falar": "Falar com o Fernando",
@@ -153,28 +152,28 @@ def carregar_cenarios():
                              }
                      },
                      "3": {
-                      "titulo": "Melhor amigo, pior inimigo",
+                      "titulo": "\nMelhor amigo, pior inimigo",
                       "descricao": "O seu amigo/inimigo Lucas Fukada acabou hackeando o elevador. Você não pode ir pro terceiro andar",
                       "opcoes": {
                               "elevador": "Voltar para o elevador"
                               }
                       },
                       "4": {
-                              "titulo": "Melhor amigo, pior inimigo",
+                              "titulo": "\nMelhor amigo, pior inimigo",
                               "descricao": "O seu amigo/inimigo Lucas Fukada acabou hackeando o elevador. Você não pode ir pro quarto andar",
                       "opcoes": {
                               "elevador": "Voltar para o elevador"
                               }
                       },
                       "5": {
-                              "titulo": "O refeitorio",
+                              "titulo": "\nO refeitorio",
                               "descricao": "Você acabou de chegar no restaurante e acha um arduíno (não se sabe porque está lá). Você recebeu o arduino!",
                               "opcoes":{
                                       "ficar": "Voce irá para as salas das entidades"
                                       }
                       },
                       "ficar": {
-                              "titulo": "Os ninjas assassinos",
+                              "titulo": "\nOs ninjas assassinos",
                               "descricao": "Você está andando pelas salas das entidades e se sente vigiado. Não tem ninguém lá, apenas você, ou era o que você achava... \n Do nada surge um ninja veterano te desafiando para uma luta",
                               "opcoes": {
                                       "lutar": "Lutar com o ninja",
@@ -182,14 +181,14 @@ def carregar_cenarios():
                                       }
                       },
                       "fugir": {
-                              "titulo": "Não escaparás!!",
+                              "titulo": "\nNão escaparás!!",
                               "descricao": "Você tenta fugir mas os ninjas não te deixam escapar",
                               "opcoes": {
                                       "lutar": "Lutar contra o ninja"
                                       }
                               },
                       "lutar": {
-                              "titulo": "Honra do ninja",
+                              "titulo": "\nHonra do ninja",
                               "descricao":"Após vencer o ninja, ele o leva para o covil do Raul. \n Agora você precisa enfrentar os golens!!",
                               "opcoes": {
                                       "lutar4": "Lutar contra os golens"
@@ -204,9 +203,9 @@ inventario = []
 def batalha():
     vida_do_inimigo  = 100
     sua_vida = 100
-    print ("Vida do inimigo: {0}".format(vida_do_inimigo))
+    print ("\nVida do inimigo: {0}".format(vida_do_inimigo))
     print ("Sua vida: {0}".format(sua_vida))
-    print ("A new foe has appeard")
+    print ("\nA NEW FOE HAS APPEARED")
     
     while vida_do_inimigo > 0:
         golpes = input("O que deseja fazer: Jogar um chinelo (entre 5 a 15 de dano) \n Dialogo (entre 10 e 12 de dano) \n Usar um item \n Cabecada (entre 20 e 30 de dano)")
@@ -265,7 +264,7 @@ def main():
             
             escolha = input("Escolha uma opção \n")
             while escolha not in cenarios:
-                print ("ação invalida")
+                print ("\nação inválida")
                 escolha = input("Escolha uma opção \n")
 
             if escolha in opcoes:
