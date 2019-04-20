63fb825a9ece5b6fa7c6d84deacf2732e939dd8c
VitorMiada <49501307+VitorMiada@users.noreply.github.com>
2019-04-20 01:03:38 -0300

    Update ep1.py

--- ep1.py
+++ ep1.py
@@ -41,30 +41,51 @@ def carregar_cenarios():
             "titulo": "Sala do sonho",
             "descricao": "Voce entrou em um aquario e acabou pegando no sono tendo um sonho no qual nao era possivel sair dele sem responder a grande pergunta: sim ou não?",
             "opcoes": {
-                "s": "sim"
+                "s": "sim",
                 "n": "nao"
             }
         },
-        "sim": {
-            "titulo": "Sebastião Camargo"
+        "s": {
+            "titulo": "Sebastião Camargo",
             "descricao":  "Você chega na sala Sebastião Camargo. /n Ihh! Você entrou no meio de uma aula do sétimo semestre de economia.",
         "opções": {
-            "voltar": "Sair de fininho e fingir que nada aconteceu"
-            "disfarce": "Colocar sua fantasia de economista e fingir que você pertence ao grupo"
+            "voltar": "Sair de fininho e fingir que nada aconteceu",
+            "disfarce": "Colocar sua fantasia de economista e fingir que você pertence ao grupo",
             "ousadia": "Falar que você é da GV e veio fazer uma consultoria"
             }
         },
-        "nao": {
-            "titulo": "L3"
+        "n": {
+            "titulo": "L3",
             "descricao" : "Você chega no L3. Os fornos para calibrar sua termorresistência estão todos nas bancadas.",
             "opções": {
-                "exemplar": "Ser um bom aluno e acabar seu relatório"
+                "exemplar": "Ser um bom aluno e acabar seu relatório",
                 "desonesto": "Descalibrar todos os fornos"
             }    
         }
-
+            }
+           
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
+inventario = []
+def batalha():
+    vida = 100
+    print ("A new foe has appeard")
+    golpes = input("O que deseja fazer: Jogar um chinelo (#entre 5 a 15 de vida), Dialogo (#entre 10 e 12), Usar um item, Cabecada (#entre 20 e 30)")# a ser desenvolvido
+    while vida > 0:
+        if golpes =="Jogar um chinelo":
+            vida-=10#random
+            print (vida)
+        if golpes =="Dialogo":
+            vida -=11#random
+            print (vida)
+        if golpes == "Usar item":
+            print (inventario)# a ser desenvolvido
+            print (vida)
+        if golpes == "Cabecada":
+            vida -=25#random
+            print (vida)
+            
+        
 
 
 def main():
